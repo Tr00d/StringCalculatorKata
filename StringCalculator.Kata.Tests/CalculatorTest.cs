5f5c7af2f7395959cbf557e316ce61d7e0604e6b
Guillaume Faas <guillaume.faas@pwc.com>
2022-06-13 08:29:26 +0200

    Initial commit

---
+++ StringCalculator.Kata.Tests/CalculatorTest.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+
+namespace StringCalculator.Kata.Tests;
+
+public class CalculatorTest
+{
+    private readonly Calculator calculator;
+
+    public CalculatorTest()
+    {
+        this.calculator = new Calculator();
+    }
+
+    [Fact]
+    public void SomeFakeTest()
+    {
+        bool value = true;
+        value.Should().BeTrue();
+    }
+}
---
+++ StringCalculator.Kata/Calculator.cs
@@ -0,0 +1,6 @@
+﻿namespace StringCalculator.Kata;
+
+public class Calculator
+{
+    public int Add(string numbers) => throw new NotImplementedException();
+}
